7304a2b80d3a72258687b5fe47ad4fb4673482a2
Etienne Bazin <etienne.bazin@wifirst.fr>
2015-12-20 19:26:19 +0100

    add router between components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,22 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router'
+
+import {ConnectToChatRoom} from "./connect-to-chat-room.component"
+import {ChatRoom} from "./chat-room.component"
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My first angular 2 app</h1>'
+  template: `
+    <h1>My very SECOND angular 2 app</h1>
+    <a [routerLink]="['ConnectToChatRoom']">Connect To Chat Room</a>
+    <a [routerLink]="['ChatRoom']">Chat Room</a>
+    <router-outlet></router-outlet>
+  `,
+  directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+  {path: '/connect', name: 'ConnectToChatRoom', component: ConnectToChatRoom},
+  {path: '/chat-room', name: 'ChatRoom', component: ChatRoom}
+])
 
 export class AppComponent{  }
