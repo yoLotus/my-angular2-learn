4e6c6cab0bda90f305a154974db92e68a10aac47
Etienne Bazin <etienne.bazin@wifirst.fr>
2015-12-21 14:27:08 +0100

    templateUrl and more complexe templates

--- app/chat-room.component.ts
+++ app/chat-room.component.ts
@@ -1,7 +1,8 @@
 import {Component} from 'angular2/core'
 
 @Component({
-  template: '<h1>Chat room</h1>'
+  // template: '<h1>Chat room</h1>'
+  templateUrl: '/static/templates/chat-room.html'
 })
 
 export class ChatRoom{}
--- app/connect-to-chat-room.component.ts
+++ app/connect-to-chat-room.component.ts
@@ -1,6 +1,10 @@
 import {Component} from 'angular2/core'
 
 @Component({
-  template: '<h1>Connection</h1>'
+  template: `
+    <h1>Connection</h1>
+    <p> To join the chat room, enter a nickname </p>
+    <input type="text">
+  `
 })
 export class ConnectToChatRoom{}
