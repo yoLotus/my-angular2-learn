6ec713dd667fa8dbfe317abaa7f2eccc2a614cdd
Etienne Bazin <etienne.bazin@wifirst.fr>
2015-12-23 09:49:08 +0100

    displaying messages through chat-room component

--- app/chat-room.component.ts
+++ app/chat-room.component.ts
@@ -1,8 +1,25 @@
 import {Component} from 'angular2/core'
 
+interface Message {
+  content: string;
+}
+
 @Component({
   // template: '<h1>Chat room</h1>'
   templateUrl: '/static/templates/chat-room.html'
 })
+export class ChatRoom{
+  constructor(){}
 
-export class ChatRoom{}
+  public messages:Message[] = MESSAGES;
+  public editedMessage:string = '';
+
+  sendMessage(){
+    this.messages.push({content: this.editedMessage});
+    this.editedMessage = '';
+  }
+}
+
+var MESSAGES: Message[] = [{content: "oui"},
+                          {content: "non"},
+                          {content: "peut-être"}];
