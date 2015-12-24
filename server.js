551f39fcc53fb3a6341cf765af51306fa2c5a1db
Etienne Bazin <etienne.bazin@wifirst.fr>
2015-12-24 18:10:19 +0100

    add static css file (included bootstrap)

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var io = require('socket.io')(http);
 
 app.use('/static/templates', express.static('./app/templates'));
 app.use('/app', express.static('./app'));
-app.use('/node_modules', express.static('./node_modules'))
+app.use('/node_modules', express.static('./node_modules'));
+app.use('/static/css', express.static('./app/css'));
+app.use('/static/vendor', express.static('./app/vendor'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -20,14 +22,11 @@ app.get('/chat-room', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
 
   socket.on('disconnect', function(){
-    console.log('a user disconnected');
   });
 
   socket.on('chat message', function( msg ){
-    console.log('message: ' + msg);
     io.emit('chat message', msg);  });
 });
 
