bf7ed525d19698558c077bd900e5dff21e0ccc69
Etienne Bazin <etienne.bazin@wifirst.fr>
2015-12-25 12:14:38 +0100

    using connection service

--- app/connect-to-chat-room.component.ts
+++ app/connect-to-chat-room.component.ts
@@ -1,13 +1,25 @@
-import {Component} from 'angular2/core'
+import {Component} from 'angular2/core';
+import {ConnectionService} from './connection.service';
 
 @Component({
-  templateUrl: 'static/templates/connect-to-chat-room.html'
+  templateUrl: 'static/templates/connect-to-chat-room.html',
+  providers: [ConnectionService]
 })
 export class ConnectToChatRoom {
   clickMessage = '';
 
+  constructor(private _connectionService: ConnectionService){}
+
+  private nickname: string;
+
   enterChatRoom(){
-    this.clickMessage = 'Please wait for connection... nous sommes le ' +
-      Date().toString();
+    this.clickMessage = 'Please wait for connection...';
+    this._connectionService.checkNicknameAvailabality(this.nickname)
+      .then(availability => {
+        if (availability)
+          this.clickMessage = 'About to enter';
+        else
+          this.clickMessage = 'This nickname is already used, take an other one';
+      });
   }
 }
